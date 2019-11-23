4d72c987e9a3a3c8ea4052e34804a0952dfebb79
Kazuya Ueoka <info@fromkk.info>
2019-11-23 14:49:34 +0900

    Initial Commit

---
+++ SpellChecker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpellChecker
+//
+//  Created by Kazuya Ueoka on 2019/11/23.
+//  Copyright © 2019 Kazuya Ueoka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
